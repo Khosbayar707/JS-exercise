a462689b6bfa4457160a0a9dd404e6dfa89961db
Khosbayar707 <torutora0415@gmail.com>
2024-11-03 22:14:58 +0800

    space

--- for.js
+++ for.js
@@ -38,9 +38,74 @@
 
 //Тооны хүрд харуулах програм бич.
 
-let number = 3;
+// let number = 3;
 
-for (let i = 1; i <= 10; i++) {
-  let urjver = number * i;
-  console.log(number, "x", i, "=", urjver);
+// for (let i = 1; i <= 10; i++) {
+//   let urjver = number * i;
+//   console.log(number, "x", i, "=", urjver);
+// }
+
+// Create a while loop that runs from 1-100
+
+// Every 10 loops, print out: Checkpoint! and the loop number
+// On loop 50 print out ONLY: Half way there!
+// On the final loop (100) print out ONLY: You made it!
+// AFTER the loop is done, print out: All, done!
+// You should see this printed out when run:
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 100 === 0) {
+//     console.log("You made it!");
+//   } else if (i % 50 === 0) {
+//     console.log("Half way there!");
+//   } else if (i % 10 === 0) {
+//     console.log("Checkpoint!", i);
+//   }
+// }
+// console.log("All, done!");
+
+/**
+Төсвийн хянагч (Budget Tracker)
+
+Та өдөр тутмын зардлаа хянахыг хүсч байна. Таны төсөв буюу зарцуулах боломжтой мөнгөний хэмжээ $100 байна.
+
+Ажлын өдрүүдэд (Даваа - Баасан) зардал $5 байх болно.
+Амралтын өдрүүдэд (Бямба, Ням) зардал $10 байх болно.
+Хэрэв  нийт зардал $80-аас илүү, гэхдээ $100-аас бага болвол "Таны нийт зардал 100$ руу ойртож байна шүү" гэж хэвлэ.
+Хэрэв  нийт зардал $100 буюу төсвийн хязгаарт хүрсэн бол: "Та зардлын хязгаарт хүрлээ!" гэж хэвлээд давталтыг зогсоо.
+
+
+budget => 100
+day =>
+totalExpense => 
+
+ */
+
+const budget = 100;
+let day = 1; // 1,2,3,4,5,6,7
+
+for (let totalExpense = 0; totalExpense <= budget; ) {
+  let dayExpense;
+
+  if (day <= 5) {
+    dayExpense = 5;
+  } else if (day > 5 && day <= 7) {
+    dayExpense = 10;
+  }
+
+  totalExpense = totalExpense + dayExpense;
+
+  if (totalExpense >= 80 && totalExpense <= 100) {
+    console.log("Таны нийт зардал 100$ руу ойртож байна шүү");
+  } else if (totalExpense > 99) {
+    console.log("Та зардлын хязгаарт хүрлээ!");
+  }
+
+  day++;
+
+  if (day >= 7) {
+    day = 1;
+  }
 }
+
+console.log(totalExpense);
